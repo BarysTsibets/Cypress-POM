b1ebbe9897fb67e801cc7577a8af722703c85396
Barys_Tsibets <molekyla_08@mail.ru>
2021-05-24 15:07:12 -0700

    go to My_address page flow

--- cypress/integration/signIn_smoke_test.js
+++ cypress/integration/signIn_smoke_test.js
@@ -63,7 +63,8 @@ describe ('SignIn test suit', function(){
 
     })
 
-    it.only('SignIn: go to My_Credit_Slips',function(){
+
+    it('SignIn: go to My_Credit_Slips',function(){
 
         cy.visit(this.data.signInPage)                                               // open SignIn Page
         sIn.enterEmailToSignIn(this.data.test_signIn_email)                          //fill email in SignIn field
@@ -71,10 +72,29 @@ describe ('SignIn test suit', function(){
         cy.get(sIn.signInBtn).should('be.visible').click()                           //click SignIn Button
       
 
-        cy.contains(sIn.My_Credit_Slips_Btn).should('be.visible').click()  //go to Order_history_and_details page
+        cy.contains(sIn.My_Credit_Slips_Btn).should('be.visible').click()            //go to My_credit_slip page
 
-        cy.title().should('eq', sIn.My_credit_slips_PageTitle)                         //assert opened page title
+        cy.title().should('eq', sIn.My_credit_slips_PageTitle)                         //assert My_credit_slip page title
 
+    })
+
+    it.only('SignIn: go to My_Address',function(){
+
+        cy.visit(this.data.signInPage)                                               // open SignIn Page
+        sIn.enterEmailToSignIn(this.data.test_signIn_email)                          //fill email in SignIn field
+        sIn.enterPasswordToSignIn(this.data.password)                                //fill password in SignIn field
+        cy.get(sIn.signInBtn).should('be.visible').click()                           //click SignIn Button
+      
+
+        cy.contains(sIn.My_addresses_Btn).should('be.visible').click()               //go to My_address_page page
+
+        cy.title().should('eq', sIn.My_address_PageTitle)                            //assert My_address page title
+
+        cy.get('.last_item').should('contain', this.data.company_name)               //assert consumer data at My Address page
+                            .and('contain', this.data.l_name)
+                            .and('contain', this.data.test_city)
+
+        cy.contains(' Back to your account').should('be.visible').click()             //return to My Account page
     })
 
 })
