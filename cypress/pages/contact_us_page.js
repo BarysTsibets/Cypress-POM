05cd1e3bc88eb8f4ba54a1140434eadb60606d22
Barys_Tsibets <molekyla_08@mail.ru>
2021-05-26 18:31:50 -0700

    contactUs smoke test Refactoring

--- cypress/pages/contact_us_page.js
+++ cypress/pages/contact_us_page.js
@@ -8,7 +8,11 @@ contact_BreadCrumb_Btn = '.navigation_page'
 contact_BreadCrumb_Text = 'Contact'
 drop_Down_btn = '#id_contact'
 drop_Down_CustomerServise_text = 'Customer service'
+drop_Down_Webmaster_text = 'Webmaster'
+customer_Service_ToolTip = 'For any question about a product, an order'
+webmaster_Service_ToolTip= 'If a technical problem occurs on this website'
 
+s
 
 
 
@@ -20,6 +24,9 @@ select_CustomerService_DropDown(){
     return cy.get(this.drop_Down_btn).select(this.drop_Down_CustomerServise_text)
 }
 
+select_Webmaster_DropDown(){
+    return cy.get(tis.drop_Down_btn).select('Webmaster')
+}
 
 
 }
