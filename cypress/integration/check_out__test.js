4ac748b3c095f5eb57c39d9cf347ed53fd6ade4f
Barys_Tsibets <molekyla_08@mail.ru>
2021-05-28 19:34:05 -0700

    hoover over event added

--- cypress/integration/check_out__test.js
+++ cypress/integration/check_out__test.js
@@ -12,7 +12,10 @@ describe('Check Out Test Suit', function(){
     it('Check Out Smoke Test', function(){
 
         cy.signIn_and_go_to_MainPage(this.data.test_signIn_email, this.data.password)
-        
+        //cy.get('.sf-menu > :nth-child(2) > .sf-with-ul').realHover()
+        cy.contains('Dresses').realHover()
+        cy.contains('Evening Dresses').click({force:true})
+
 
     })
 
