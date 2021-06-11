8f2aa93bc62760bdab933681315340761a93ceef
Barys_Tsibets <molekyla_08@mail.ru>
2021-06-11 15:56:19 -0700

    minor refactoring

--- cypress/integration/add_to_cart_test.js
+++ cypress/integration/add_to_cart_test.js
@@ -15,37 +15,36 @@ describe('Check Out Test Suit', function(){
 
     it('Add to Cart Smoke Test', function(){
 
-        //User LogIn first
-
-        cy.signIn_and_go_to_MainPage(this.data.test_signIn_email, this.data.password)
+        cy.signIn_and_go_to_MainPage(this.data.test_signIn_email, this.data.password)      //Log In and go to Main page flow 
         //cy.get('.sf-menu > :nth-child(2) > .sf-with-ul').realHover()
-        cy.contains('Dresses').realHover()
-        cy.contains('Evening Dresses').click({force:true})
+        cy.contains('Dresses').realHover()                                      //hover over Dresses
+        cy.contains('Evening Dresses').click({force:true})                      // click on Evening Dresses in appeared window
 
-        ptc.dress_Product().click()                             //Select Item
+        ptc.dress_Product().click()                                             //Select Item
         //cy.get('.right-block > h5 > .product-name').click()  
 
-        ptc.increase_Prod_Qty_By_One()                          //increase Qty of product (+1)
+        ptc.increase_Prod_Qty_By_One()                                          //increase Qty of product (+1)
         //cy.get('.button-plus > span').click()               
 
-        ptc.size_Drop_Down_Menu().select('M').should('have.value', '2')   // select M size from Drop Down
+        ptc.size_Drop_Down_Menu().select('M').should('have.value', '2')        // select M size from Drop Down
         //cy.get('#group_1').select('M').should('have.value', '2')   
 
-        ptc.change_Product_Color()      //select Pink color
+        ptc.change_Product_Color()                                             //select Pink color
         //cy.get('#color_24').click()   
 
-        ptc.pink_Dress_Img().should('be.visible')                   // Img with Pink Dress presented
+        ptc.pink_Dress_Img().should('be.visible')                              // Img with Pink Dress presented
         //cy.get('#thumb_11').should('be.visible')   
 
-        ptc.click_Add_to_Cart_Btn()                         // Click "Add to Cart" Button
+        ptc.click_Add_to_Cart_Btn()                                            // Click "Add to Cart" Button
         //cy.get('.exclusive > span').click()       
 
-        ptc.click_process_to_Checkout_Btn()                 // click Process CheckOut Button
+        ptc.click_process_to_Checkout_Btn()                                    // click Process CheckOut Button
         //cy.get('.button-medium > span').click()   
 
-        cy.title().should('eq', 'Order - My Store')            // assert cart page title 
+                    //Assertions//
+        cy.title().should('eq', 'Order - My Store')                            // assert cart page title 
 
-        cy.get('.cart_description > .product-name > a').should('be.visible')
+        cy.get(ptc.product_in_cart).should('be.visible')
     })
     
 
--- cypress/pages/product_to_cart_page.js
+++ cypress/pages/product_to_cart_page.js
@@ -9,36 +9,29 @@ pink_color_btn ='#color_24'
 pink_Dress_icon = '#thumb_11'
 add_to_cart_btn = '.exclusive > span'
 process_to_checkout_btn = '.button-medium > span'
+product_in_cart = '.cart_description > .product-name > a'
 
 
 dress_Product(){
-    return cy.get(this.printed_Dress)
-}    
+    return cy.get(this.printed_Dress)}    
  
 increase_Prod_Qty_By_One(){
-    return cy.get(this.qty_increase_btn).click()
-}
+    return cy.get(this.qty_increase_btn).click()}
 
 size_Drop_Down_Menu(){
-    return cy.get(this.drop_down_size)
-}
+    return cy.get(this.drop_down_size)}
 
 change_Product_Color(){
-    return cy.get(this.pink_color_btn).click()
-}
+    return cy.get(this.pink_color_btn).click()}
 
 pink_Dress_Img(){
-    return cy.get(this.pink_Dress_icon)
-}
+    return cy.get(this.pink_Dress_icon)}
 
 click_Add_to_Cart_Btn(){
-    return cy.get(this.add_to_cart_btn).click()
-}
+    return cy.get(this.add_to_cart_btn).click()}
 
 click_process_to_Checkout_Btn(){
-    return cy.get(this.process_to_checkout_btn).click()
-}
-
+    return cy.get(this.process_to_checkout_btn).click()}
 }
 
 export default ProductToCart
