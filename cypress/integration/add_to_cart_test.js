8dcdeebd012db53fb5e705535530a37cb96265f6
Barys_Tsibets <molekyla_08@mail.ru>
2021-06-14 19:45:41 -0700

    processToCheckout go to Adress page

--- cypress/integration/add_to_cart_test.js
+++ cypress/integration/add_to_cart_test.js
@@ -47,4 +47,30 @@ describe('Check Out Test Suit', function(){
         cy.get(ptc.product_in_cart).should('be.visible')
     })
     
+    it.only('CheckOut Smoke Test', function(){
+
+                        //Add item to the cart flow
+        cy.signIn_and_go_to_MainPage(this.data.test_signIn_email, this.data.password)      //Log In and go to Main page flow
+        cy.contains('Dresses').realHover()                                      //hover over Dresses
+        cy.contains('Evening Dresses').click({force:true})                      // click on Evening Dresses in appeared window
+        ptc.dress_Product().click()                                             //Select Item
+        ptc.increase_Prod_Qty_By_One()                                          //increase Qty of product (+1)
+        ptc.size_Drop_Down_Menu().select('M').should('have.value', '2')        // select M size from Drop Down
+        ptc.change_Product_Color()                                             //select Pink color
+        ptc.pink_Dress_Img().should('be.visible')                              // Img with Pink Dress presented
+        ptc.click_Add_to_Cart_Btn()                                            // Click "Add to Cart" Button
+        ptc.click_process_to_Checkout_Btn()                                    // click Process CheckOut Button
+                    //Assertions//
+        cy.title().should('eq', 'Order - My Store')                            // assert cart page title 
+        cy.get(ptc.product_in_cart).should('be.visible')
+
+                       //Check Out flow
+        cy.scrollTo('bottom')
+        cy.get('.cart_navigation > .button > span').should('be.visible').click()           //click Process To CheckOuy Button
+        cy.get('.navigation_page').should('have.text', 'Addresses').should('be.visible')   //assertr that Adress page presented
+
+    })
+
+
+
 })
